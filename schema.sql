4952171b0cf3822daf8187a31d8275ba5bb0f174
Dunkrick <rithwickgurram05@gmail.com>
2026-03-18 11:40:19 +0530

    feat: Initial Clean Plate

---
+++ schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Scenes (
+    scene_id INT PRIMARY KEY, 
+    title TEXT NOT NULL, 
+    location TEXT, 
+    shoot_date DATE
+);
+
+CREATE TABLE Shots (
+    shot_id INT PRIMARY KEY,           
+    scene_id INT,                      
+    description TEXT, 
+    shot_type TEXT CHECK (shot_type IN ('A-Roll', 'B-Roll', 'Screen Recording')), 
+    frame_rate INT, 
+    Shot_status BOOLEAN DEFAULT FALSE,
+    FOREIGN KEY (scene_id) REFERENCES Scenes(scene_id)
+);
